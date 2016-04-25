7868c6ad0053ff14df8f4e8281e1267bb45efdaa
Terry Capan <tcapanjr@gmail.com>
2016-04-24 22:42:45 -0500

    check routes

--- server/routes.js
+++ server/routes.js
@@ -182,6 +182,7 @@ module.exports = function(app) {
   app.route('/logout')
     .get(function(req, res) {
       req.session.destroy(function(err) {
+        console.log('here - ', req.session);
         sendResponse(res, err, {}, 200);
       });
     });
